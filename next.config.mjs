21cb9f13f79c4dcd0808409aee0f006fa8abc43a
bramastyapp <bramastyapp@gmail.com>
2024-09-22 10:43:58 +0700

    feat: listing showcase integration with backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [],
+    remotePatterns: [
+      {
+        hostname: `${process.env.NEXT_PUBLIC_STORAGE_BASE_URL}/*`,
+      },
+    ],
   },
 };
 
