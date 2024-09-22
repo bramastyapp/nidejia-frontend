93b9353c9f313041d0a28cbf27b7b22e8ca5bb79
bramastyapp <bramastyapp@gmail.com>
2024-09-22 10:14:29 +0700

    feat: setup auth with NextAuth

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -18,6 +18,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useRouter } from "next/navigation";
 import { useToast } from "@/components/atomics/use-toast";
 import { useRegisterMutation } from "@/services/auth-service";
+import { signIn } from "next-auth/react";
 
 const schema = yup.object().shape({
   name: yup.string().min(5).required(),
@@ -50,14 +51,24 @@ function SignUp() {
         password_confirmation: values.password,
       }).unwrap();
 
-      console.log("🚀 ~ onSubmit ~ res:", res);
+      if (res.success) {
+        const user = res.data;
+        await signIn("credentials", {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          token: user.token,
+          redirect: false,
+        });
 
-      form.reset();
-      toast({
-        title: "Welcome",
-        description: "Sign up successfully",
-        open: true,
-      });
+        toast({
+          title: "Welcome",
+          description: "Sign up successfully",
+          open: true,
+        });
+
+        router.push("/");
+      }
     } catch (error: any) {
       toast({
         title: "Something when wrong!",
